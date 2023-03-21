2627c5c106e4b82eceb716353685e27088e1d762
AlainBeckerCPE <alain.becker@cpe.fr>
2023-03-21 11:53:04 +0100

    init
    Maven scaffolding using
    mvn archetype:generate \
      -DarchetypeArtifactId=maven-archetype-quickstart \
      -DarchetypeVersion=1.4 \
      -DarchetypeCatalog=https://repo.maven.apache.org/maven2/archetype-catalog.xml \
      -DinteractiveMode=false \
      -DgroupId=fr.cpe.cpe2223 \
      -DartifactId=irc4chessbase \
      -Dpackage=launcher

---
+++ src/main/java/launcher/App.java
@@ -0,0 +1,13 @@
+package launcher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
